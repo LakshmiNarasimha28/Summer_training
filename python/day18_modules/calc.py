be440eee77d2c50f229f42589d55b378facb6ca5
Lakshmi Narasimha <150878853+LakshmiNarasimha28@users.noreply.github.com>
2026-08-03 10:52:50 +0530

    Create a simple calculator module with basic operations

---
+++ python/day18_modules/calc.py
@@ -0,0 +1,17 @@
+print("This is a simple calculator module.")
+
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    return a / b
+
+def square(a):
+    return a * a
+
